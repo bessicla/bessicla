7364c79550da956eeff5b57a59858ddc55e9f2cd
Luca <bessilu.de@gmail.com>
2019-10-17 03:33:17 -0700

    ngfg

--- gallerypic.js
+++ gallerypic.js
@@ -1,8 +1,16 @@
-$('.masonry').masonry({
+// init Masonry
+var $grid = $('.masonry').masonry({
   // options
     columnWidth: '.grid-sizer',
   itemSelector: '.cover',
     transitionDuration: 0, 
   "percentPosition": true 
   
-});   
+});
+
+
+$grid.on( 'click', '.cover', function() {
+  // change size of item via class
+  $( this ).toggleClass('.masonry');
+  $grid.masonry();
+});

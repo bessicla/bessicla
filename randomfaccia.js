3263df7498ebba74a826321608b8fad2bf7dd7af
Claudia <bessicla@gmail.com>
2019-12-30 19:56:51 -0800

    up home

--- randomfaccia.js
+++ randomfaccia.js
@@ -6,13 +6,18 @@
                 "logogif/logo1.gif",
                 "logogif/logo2.gif",
                 "logogif/logo3.gif",
-                "logogif/logo4.gif"
+                "logogif/logo4.gif",
+                "logogif/logo5.gif"
             ];
 
     function randImg() {
         var size = images.length
         var x = Math.floor(size * Math.random())
-        document.getElementById('testa').src = images[x];
+        document.getElementById('testa1').src = images[x];
+        var x = Math.floor(size * Math.random())
+        document.getElementById('testa2').src = images[x];
+        var x = Math.floor(size * Math.random())
+        document.getElementById('testa3').src = images[x];
     };
 $(document).ready(function () {
     randImg();
@@ -22,6 +27,6 @@ $(document).ready(function () {
 $(window).on('load', function () {
     setTimeout(function () {
         $("#hideAll").fadeOut(1000)
-    }, 1000);   
+    }, 800);   
     
 });
